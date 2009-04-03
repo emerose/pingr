51390b5f39773858d947257d9a025a436f25525a
Sam Quigley <sq@wesabe.com>
2009-04-03 00:05:23 -0700

    echo test of pingdom api

--- pingr.rb
+++ pingr.rb
@@ -1,1 +1,19 @@
-#!/bin/env ruby
+#!/usr/bin/env ruby
+#
+
+require 'rubygems'
+gem 'soap4r'
+require 'PingdomAPIDriver'
+
+load File.join(File.dirname(__FILE__), "creds.rb")
+
+creds = Auth_CredentialsData.new
+creds.username = USERNAME
+creds.password = PASSWORD
+
+pingdom = PingdomAPISoapPort.new
+res = pingdom.auth_login(API_KEY, creds)
+raise "#{res}" unless res.status == 0
+session = res.sessionId
+res = pingdom.test_echo("HI")
+puts res.outString
